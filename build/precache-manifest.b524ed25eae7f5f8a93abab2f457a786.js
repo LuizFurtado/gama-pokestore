90a6c40a6c7e56355a37166e6cb3cc66cb365b4c
Luiz Furtado <luizfpfurtado@hotmail.com>
2020-07-08 20:03:02 -0300

    adicionada build do projeto

---
+++ build/precache-manifest.b524ed25eae7f5f8a93abab2f457a786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc746dc9f77c119d3797c7edd756e28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e11c2b5ff4cf44eb8de4",
+    "url": "/static/css/main.dbb0e5c7.chunk.css"
+  },
+  {
+    "revision": "8126aef1874ca6094f1f",
+    "url": "/static/js/2.db1c2007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db1c2007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11c2b5ff4cf44eb8de4",
+    "url": "/static/js/main.fbe655ed.chunk.js"
+  },
+  {
+    "revision": "1308ce9a4d9b86ead68d",
+    "url": "/static/js/runtime-main.8135f19d.js"
+  },
+  {
+    "revision": "14e45ff48c73ed081ba0a01007f0e8bc",
+    "url": "/static/media/sand-with-waves-dunes.14e45ff4.jpg"
+  }
+]);
