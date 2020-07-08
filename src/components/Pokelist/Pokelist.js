d896ddf0d095558142b4481228417dc263d0522b
Furtado Luiz <Luiz.Furtado@andritz.com>
2020-07-08 10:26:29 -0300

    adicionados cards para apresentação dos pokemons

--- src/components/Pokelist/Pokelist.js
+++ src/components/Pokelist/Pokelist.js
@@ -2,14 +2,24 @@ import React from 'react'
 
 export default function Pokelist({ pokedata }) {
   return (
-    <div>
-      {
-        pokedata.map((pokemon, index) => (
-          <div key={ `${pokemon.name}-${index}` }>
-            { pokemon.name } - { pokemon.id } - { pokemon.price }
-          </div>
-        ))
-      }
-    </div>
+    <section className="container">
+      <div className="row">
+        {
+          pokedata.map((pokemon, index) => (
+            // <div key={ `${pokemon.name}-${index}` }>
+            //   { pokemon.name } - { pokemon.id } - { pokemon.price }
+            // </div>
+            <div className="card" key={ `${pokemon.name }-${ index }` } style={{width: 18 + 'rem'}}>
+              <img src={ pokemon.image } className="card-img-top" alt="Imagem do pokemon { pokemon.name }" />
+              <div className="card-body">
+                <h5 className="card-title">{ pokemon.name }</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </section>
   );
 };
